8656bc5dd032c3f53541104cf0bbaea7fa29dd78
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-04-23 19:07:06 +0000

    pascal's triangle removing the test part

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -30,18 +30,3 @@ def pascal_triangle(n):
         #assign each eline to triangle
         triangle.append(line)
     return triangle
-
-
-
-
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
-
