6838b83e83d3b16c1282ae244edbb7b1786154eb
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-04-30 23:43:34 +0000

    lock box 4.0 refactoring

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-    Collection module
-"""
-from collections import deque
-
 
 """
     Lockboxes
@@ -14,16 +9,18 @@ def canUnlockAll(boxes):
     """
     This function is a method that determines if all the boxes can be opened
     """
+    visited_boxes = set([0])
+    unlocked_boxes = [0]
 
-    n = len(boxes)
-    visited = set([0])
-    queue = deque([0])
+    while unlocked_boxes:
+        # idx for the current boxe
 
-    while queue:
-        box = queue.popleft()
-        for key in boxes[box]:
-            if key not in visited and key < n:
-                visited.add(key)
-                queue.append(key)
+        idx = unlocked_boxes.pop(0)
+        # retrieve the current box and check all the key
 
-    return len(visited) == n
+        for key in boxes[idx]:
+            if key not in visited_boxes and key < len(boxes):
+                visited_boxes.add(key)
+                unlocked_boxes.append(key)
+
+    return len(visited_boxes) == len(boxes)
