0d648eb190c50d78531721f93dcc2eaf930c0ff3
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-05-08 22:03:17 +0000

    minimum operation 2.0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,20 +12,27 @@ def minOperations(n):
       The number for which, we are searching the minimum operation
     """
     # this variable count tne number of 'H' in the file
-    counter = 1
+    h_counter = 1
     # count the number of operation
-    operation = 0
+    ops_counter = 0
+    copy = 0
 
     if not isinstance(n, int):
         return 0
-    elif n == 1:
-        return 0
 
-    while counter < n:
-        # count copy operation
-        if n % counter == 0:
-            operation += 1
-        # count past operation
-        counter *= 2
-        operation += 1
-    return operation
+    while h_counter < n:
+       # first copy
+        if copy == 0:
+           copy = h_counter
+           h_counter += copy
+           ops_counter += 2
+        # copy all and past
+        elif n - h_counter > 0 and (n - h_counter) % h_counter == 0:
+            copy = h_counter
+            h_counter += copy
+            ops_counter += 2
+        # simple past
+        elif copy > 0:
+            h_counter += copy
+            ops_counter += 1
+    return ops_counter
