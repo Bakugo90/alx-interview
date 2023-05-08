dd73add2f0dd2a849c3a46aeda4e62e22d4a048b
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-05-08 22:12:30 +0000

    fixing PEP8 error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,11 +6,12 @@
 
 def minOperations(n):
     """a method that calculates the fewest number of operations
-      needed to result in exactly n H characters in the file.
+    needed to result in exactly n H characters in the file.
 
-      Args:
-      The number for which, we are searching the minimum operation
+    Args:
+    The number for which, we are searching the minimum operation
     """
+
     # this variable count tne number of 'H' in the file
     h_counter = 1
     # count the number of operation
@@ -21,11 +22,11 @@ def minOperations(n):
         return 0
 
     while h_counter < n:
-       # first copy
+        # first copy
         if copy == 0:
-           copy = h_counter
-           h_counter += copy
-           ops_counter += 2
+            copy = h_counter
+            h_counter += copy
+            ops_counter += 2
         # copy all and past
         elif n - h_counter > 0 and (n - h_counter) % h_counter == 0:
             copy = h_counter
